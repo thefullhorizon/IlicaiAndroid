e336ec1ae57a97a4a26f39a2413af87c37e49c1b
Gerry <gerry@superjia.com>
2017-07-21 17:55:47 +0800

    资产

--- AilicaiAndroid/src/main/java/com/ailicai/app/ui/view/IncomeDetailParentFragment.java
+++ AilicaiAndroid/src/main/java/com/ailicai/app/ui/view/IncomeDetailParentFragment.java
@@ -1,8 +1,8 @@
 package com.ailicai.app.ui.view;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,7 +10,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import com.ailicai.app.R;
 import com.ailicai.app.ui.base.BaseBindFragment;
 import com.ailicai.app.widget.NoScrollViewPager;
-import com.ailicai.app.widget.slidingtab.SlidingTabLayout;
 
 import java.util.ArrayList;
 
@@ -21,18 +20,19 @@ import butterknife.Bind;
  * Created by David on 16/1/4
  * Modified by Owen on 16/8/10
  */
-public class IncomeDetailParentFragment extends BaseBindFragment {
+public class IncomeDetailParentFragment extends BaseBindFragment implements TabLayout.OnTabSelectedListener {
 
     public static final String TAG_TYPE = "tag_type";
     public static final String TAG_REGULAR = "tag_regular";
     public static final String TAG_TRY = "tag_try";
 
-    @Bind(R.id.sliding_tabs)
-    SlidingTabLayout mSlidingTabLayout;
+    @Bind(R.id.tab_layou)
+    TabLayout mTabLayout;
     @Bind(R.id.view_pager)
     NoScrollViewPager mViewPager;
 
     private OurViewPagerAdapter mViewPagerAdapter;
+    private String[] pageTitles = new String[]{"定期资产", "体验宝"};
 
     @Override
     public int getLayout() {
@@ -41,35 +41,46 @@ public class IncomeDetailParentFragment extends BaseBindFragment {
 
     @Override
     public void init(Bundle savedInstanceState) {
-        super.init(savedInstanceState);
-
         inittabView();
-        mViewPager.setCurrentItem(getArguments().getString(TAG_TYPE).equals(TAG_REGULAR) ? 0 : 1);
     }
 
-    public void inittabView() {
-        mSlidingTabLayout.setCustomTabView(R.layout.tab_indicator2, android.R.id.text1);
-        Resources res = getResources();
-        mSlidingTabLayout.setSelectedIndicatorColors(res.getColor(R.color.main_red_color));// 设置下面的线颜色
-        mSlidingTabLayout.setDistributeEvenly(true);
-        iniTab();
-    }
-
-    private void iniTab() {
-        mViewPagerAdapter = new OurViewPagerAdapter(getActivity(), getChildFragmentManager());
+    private void inittabView() {
+        //设置TabLayout标签的显示方式
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        mTabLayout.setTabMode(TabLayout.MODE_FIXED);
+        for (String tab : pageTitles) {
+            mTabLayout.addTab(mTabLayout.newTab().setText(tab));
+        }
+        mTabLayout.setOnTabSelectedListener(this);
 
         Bundle bundle = new Bundle();
         bundle.putInt(IncomeDetailChildFragment.INCOME_TYPE, IncomeDetailChildFragment.INCOME_REGULAR);
-        mViewPagerAdapter.addNvgItem("定期资产", IncomeDetailChildFragment.class, bundle);
+        mViewPagerAdapter.addNvgItem(pageTitles[0], IncomeDetailChildFragment.class, bundle);
 
         Bundle bundle2 = new Bundle();
         bundle2.putInt(IncomeDetailChildFragment.INCOME_TYPE, IncomeDetailChildFragment.INCOME_DEMAND);
-        mViewPagerAdapter.addNvgItem("体验宝", IncomeDetailChildFragment.class, bundle2);
+        mViewPagerAdapter.addNvgItem(pageTitles[1], IncomeDetailChildFragment.class, bundle2);
 
         mViewPager.setAdapter(mViewPagerAdapter);
         mViewPager.setCanScroll(true);
         mViewPager.setOffscreenPageLimit(2);
-        mSlidingTabLayout.setViewPager(mViewPager);
+        mViewPager.setCurrentItem(getArguments().getString(TAG_TYPE).equals(TAG_REGULAR) ? 0 : 1);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
 
     private class OurViewPagerAdapter extends FragmentPagerAdapter {
