68cc47d2f2e455372687c219ce987e9c19596706
Gerry <gerry@superjia.com>
2017-09-20 15:47:59 +0800

    客服电话设置

--- AilicaiAndroid/src/main/java/com/ailicai/app/ui/mine/ContactUsActivity.java
+++ AilicaiAndroid/src/main/java/com/ailicai/app/ui/mine/ContactUsActivity.java
@@ -1,15 +1,23 @@
 package com.ailicai.app.ui.mine;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import com.ailicai.app.R;
 import com.ailicai.app.ui.base.BaseBindActivity;
+import com.ailicai.app.ui.login.UserInfo;
+
+import butterknife.Bind;
 
 /**
  * Created by Gerry on 2017/7/18.
  */
 
 public class ContactUsActivity extends BaseBindActivity {
+
+    @Bind(R.id.server_tel)
+    TextView serverTel;
 
     @Override
     public int getLayout() {
@@ -18,6 +26,8 @@ public class ContactUsActivity extends BaseBindActivity {
 
     @Override
     public void init(Bundle savedInstanceState) {
+        String serverNumber = UserInfo.getInstance().getServicePhoneNumber();
+        serverTel.setText(Html.fromHtml(getResources().getString(R.string.mine_service_tell_txt, serverNumber)));
     }
 
 
